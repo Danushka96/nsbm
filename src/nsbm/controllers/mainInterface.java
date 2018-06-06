c5f2fe3fba3fec53c7557908c48bc318e1a01857
danushka96 <danushkaherath96@gmail.com>
2018-06-06 13:05:16 +0530

    fix features in dashboard

--- src/nsbm/controllers/mainInterface.java
+++ src/nsbm/controllers/mainInterface.java
@@ -2,11 +2,26 @@ package nsbm.controllers;
 
 import com.jfoenix.controls.JFXButton;
 import javafx.fxml.FXML;
+import javafx.scene.control.Label;
 import javafx.scene.layout.AnchorPane;
+import javafx.scene.text.Text;
 
 import java.awt.event.ActionEvent;
+import java.sql.*;
+import java.text.SimpleDateFormat;
+import java.time.Duration;
+import java.util.Calendar;
 
 public class mainInterface {
+
+    @FXML
+    public Text systemtime;
+
+    @FXML
+    public Label studentcount;
+
+    @FXML
+    public Label lecturecount;
 
     @FXML
     private AnchorPane menuuni;
@@ -44,6 +59,11 @@ public class mainInterface {
     @FXML
     private JFXButton btnlogout;
 
+    public void initialize() throws SQLException {
+        settime();
+        setDashboard();
+    }
+
     @FXML
     public void handleButtonAction(javafx.event.ActionEvent actionEvent) {
         if(actionEvent.getSource()==btnhome){
@@ -67,5 +87,31 @@ public class mainInterface {
         else if(actionEvent.getSource()==btnlogout){
             System.out.println("Logout Command");
         }
+    }
+
+    private void settime(){
+//        Thread.sleep(1000);
+        Calendar cal= Calendar.getInstance();
+        SimpleDateFormat sdf=new SimpleDateFormat("yyyy/MM/dd");
+        String time=sdf.format(cal.getTime());
+        systemtime.setText(time);
+    }
+    private void setDashboard() throws SQLException {
+        int students=0,lecturers=0;
+        Connection con = ConnectionManager.getConnection();
+        String squery="SELECT * FROM students";
+        String lquery="SELECT * FROM lecturers";
+        Statement sq=con.prepareStatement(squery);
+        Statement lq=con.prepareStatement(lquery);
+        ResultSet rlq=((PreparedStatement) lq).executeQuery();
+        ResultSet rsq=((PreparedStatement) sq).executeQuery();
+        while (rsq.next()){
+            students++;
+        }
+        while (rlq.next()){
+            lecturers++;
+        }
+        studentcount.setText(Integer.toString(students));
+        lecturecount.setText(Integer.toString(lecturers));
     }
 }
